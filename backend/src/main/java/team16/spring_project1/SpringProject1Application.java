427cae58cc56b7db9d93315ee2bf0c9381963670
solji <solji.choi89@gmail.com>
2025-01-14 18:00:10 +0900

    create product entity

--- backend/src/main/java/team16/spring_project1/SpringProject1Application.java
+++ backend/src/main/java/team16/spring_project1/SpringProject1Application.java
@@ -2,15 +2,15 @@ package team16.spring_project1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringProject1Application {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringProject1Application.class, args);
-
-		System.out.println("테스트");
 	}
 
 }
