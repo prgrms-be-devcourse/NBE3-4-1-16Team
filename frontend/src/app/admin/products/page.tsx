efb05b021cb9da982ca0afbb077e61dae445a88b
solji <solji.choi89@gmail.com>
2025-01-16 16:22:53 +0900

    feat: create createPage

--- frontend/src/app/admin/products/page.tsx
+++ frontend/src/app/admin/products/page.tsx
@@ -2,9 +2,13 @@ import client from '@/lib/backend/client'
 import ClientPage from './ClientPage'
 
 export default async function Page() {
+  const response = await client.GET('/products')
+
+  const responseBody = response.data!!
+
   return (
     <>
-      <ClientPage />
+      <ClientPage responseBody={responseBody} />
     </>
   )
 }
