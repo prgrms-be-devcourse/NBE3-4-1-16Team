9ef4bec6a56df971d202bb3d0c0adee42321b1d5
solji <solji.choi89@gmail.com>
2025-01-16 14:25:06 +0900

    feat: made schema

---
+++ backend/src/main/java/team16/spring_project1/global/initData/DevInitData.java
@@ -0,0 +1,23 @@
+package team16.spring_project1.global.initData;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import team16.spring_project1.standard.util.Ut;
+
+@Profile("dev")
+@Configuration
+@RequiredArgsConstructor
+public class DevInitData {
+    @Bean
+    public ApplicationRunner devInitDataApplicationRunner() {
+        return args -> {
+            Ut.file.downloadByHttp("http://localhost:8080/v3/api-docs/apiV1", ".");
+
+            String cmd = "yes | npx --package typescript --package openapi-typescript openapi-typescript apiV1.json -o ../frontend/src/lib/backend/apiV1/schema.d.ts";
+            Ut.cmd.runAsync(cmd);
+        };
+    }
+}
