114ce307266a59a2717337881263bcceebe04422
solji <solji.choi89@gmail.com>
2025-01-16 16:10:20 +0900

    feat: list image

--- frontend/src/app/admin/ClientPage.tsx
+++ frontend/src/app/admin/ClientPage.tsx
@@ -13,15 +13,15 @@ export default function ClientPage({
       <ul>
         {responseBody.content?.map((item) => (
           <li key={item.id} className="border-[2px] border-[red] my-3">
-            <Link className="block" href={`/post/${item.id}`}>
-              <div>id : {item.id}</div>
-              <div>createDate : {item.createDate}</div>
-              <div>modifyDate : {item.modifyDate}</div>
-              <div>authorId : {item.category}</div>
-              <div>authorName : {item.productName}</div>
-              <div>title : {item.price}</div>
-              <div>published : {`${item.imageUrl}`}</div>
-            </Link>
+            <div>id : {item.id}</div>
+            <div>createDate : {item.createDate}</div>
+            <div>modifyDate : {item.modifyDate}</div>
+            <div>category : {item.category}</div>
+            <div>productName : {item.productName}</div>
+            <div>price : {item.price}</div>
+            <div>
+              <img src={`${item.imageUrl}`} alt="" />
+            </div>
           </li>
         ))}
       </ul>
