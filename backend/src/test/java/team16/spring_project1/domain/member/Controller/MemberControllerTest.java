834472c08ef4d11a6c9326ea4fc5545274d6b5d1
solji <solji.choi89@gmail.com>
2025-01-17 18:47:20 +0900

    test: login 실패 테스트 추가

--- backend/src/test/java/team16/spring_project1/domain/member/Controller/MemberControllerTest.java
+++ backend/src/test/java/team16/spring_project1/domain/member/Controller/MemberControllerTest.java
@@ -62,4 +62,104 @@ public class MemberControllerTest {
                 .andExpect(jsonPath("$.content.item.modifyDate").value(Matchers.startsWith(member.getModifyDate().toString().substring(0, 23))))
                 .andExpect(jsonPath("$.content.apiKey").value(member.getApiKey()));
     }
+
+    @Test
+    @DisplayName("로그인 without username")
+    void t2() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(post("/members/login")
+                        .content("""
+                                {
+                                    "username": "",
+                                    "password": "1234"
+                                }
+                                """.stripIndent())
+                        .contentType(
+                                new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                        )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("login"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("username-NotBlank-must not be blank"));
+    }
+
+    @Test
+    @DisplayName("로그인 without password")
+    void t3() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(post("/members/login")
+                        .content("""
+                                {
+                                    "username": "admin",
+                                    "password": ""
+                                }
+                                """.stripIndent())
+                        .contentType(
+                                new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                        )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("login"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("password-NotBlank-must not be blank"));
+    }
+
+    @Test
+    @DisplayName("로그인 wrong username")
+    void t4() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(post("/members/login")
+                        .content("""
+                                {
+                                    "username": "user0",
+                                    "password": "1234"
+                                }
+                                """.stripIndent())
+                        .contentType(
+                                new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                        )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("login"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("존재하지 않는 사용자입니다."));
+    }
+
+    @Test
+    @DisplayName("로그인 wrong password")
+    void t5() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(post("/members/login")
+                        .content("""
+                                {
+                                    "username": "admin",
+                                    "password": "1111"
+                                }
+                                """.stripIndent())
+                        .contentType(
+                                new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                        )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("login"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("비밀번호가 일치하지 않습니다."));
+    }
 }
